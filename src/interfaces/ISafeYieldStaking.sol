7b1f766ca8a8c5ea6494e2c90f75cb4fe0876cf4
ooglec <delalie@reallygreattech.com>
2024-05-06 15:59:50 +0100

    feat: Add ISafeYieldStaking interface for staking and claiming rewards

---
+++ src/interfaces/ISafeYieldStaking.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+interface ISafeYieldStaking {
+    function stake(uint128 amount) external;
+    function unstake(uint128 amount) external;
+    function claimRewards() external;
+    function getStakedBalance(address user) external view returns (uint128);
+    function getRewardsBalancse(
+        address user
+    ) external view returns (uint128, uint128);
+}
