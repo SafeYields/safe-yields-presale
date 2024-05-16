15c777611523c3ab3fd32ce2fef147b25c11534b
raiyanmook27 <raiyan.mook27@gmail.com>
2024-05-16 01:19:21 +0000

    chore: Update SafeYieldBaseTest contract with new addresses and minting logic

--- test/SafeYieldBaseTest.t.sol
+++ test/SafeYieldBaseTest.t.sol
@@ -2,7 +2,6 @@
 
 pragma solidity 0.8.21;
 import {Test, console} from "forge-std/Test.sol";
-import {SafeYieldPresale} from "src/SafeYieldPresale.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeMockToken} from "./mocks/SafeMockToken.sol";
 import {USDCMockToken} from "./mocks/USDCMockToken.sol";
@@ -10,13 +9,15 @@ import {SafeYieldRewardDistributor} from "src/SafeYieldRewardDistributor.sol";
 import {SafeYieldPresale} from "src/SafeYieldPresale.sol";
 import {SafeYieldStaking} from "src/SafeYieldStaking.sol";
 
-contract SafeYieldBaseTest is Test {
+abstract contract SafeYieldBaseTest is Test {
     uint256 public constant PRE_SALE_MAX_SUPPLY = 2_000_000e18;
     uint256 public constant STAKING_MAX_SUPPLY = 11_000_000e18;
 
     address public teamOperations = makeAddr("teamOperations");
     address public usdcBuyback = makeAddr("usdcBuyback");
     address public protocolAdmin = makeAddr("protocolAdmin");
+    address public ALICE = makeAddr("alice");
+    address public BOB = makeAddr("bob");
 
     SafeYieldRewardDistributor public distributor;
     SafeYieldPresale public presale;
@@ -31,23 +32,23 @@ contract SafeYieldBaseTest is Test {
         safeToken = new SafeMockToken("SafeToken", "SAFE", 18);
         sSafeToken = new USDCMockToken("sSafeToken", "sSAFE", 18);
 
+        staking = new SafeYieldStaking(
+            address(safeToken),
+            address(sSafeToken),
+            address(usdc),
+            protocolAdmin
+        );
+
         presale = new SafeYieldPresale(
             address(safeToken),
             address(usdc),
             address(staking),
             uint128(PRE_SALE_MAX_SUPPLY),
             1000e18,
-            10_000e18,
-            1e6,
-            1e6,
-            1e18
-        );
-
-        staking = new SafeYieldStaking(
-            address(safeToken),
-            address(sSafeToken),
-            address(usdc),
-            protocolAdmin
+            100_000e18,
+            1e18,
+            5_00,
+            5_00
         );
 
         distributor = new SafeYieldRewardDistributor(
@@ -65,6 +66,13 @@ contract SafeYieldBaseTest is Test {
         safeToken.setMinterLimit(address(distributor), STAKING_MAX_SUPPLY);
         safeToken.setMinterLimit(address(presale), PRE_SALE_MAX_SUPPLY);
 
+        _mintUsdc2Users();
+
         vm.stopPrank();
+    }
+
+    function _mintUsdc2Users() internal {
+        usdc.mint(ALICE, 110_000e6);
+        usdc.mint(BOB, 100_000e6);
     }
 }
