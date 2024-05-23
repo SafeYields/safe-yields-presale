2650aa637e017cf6489a22047043b6d1513c69cd
JayusJay <atayure@gmail.com>
2024-05-23 13:10:54 +0000

    chore!: minor adjustments + left recommendations

--- src/SafeToken.sol
+++ src/SafeToken.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.21;
+pragma solidity 0.8.26;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
-import {ISafeToken} from "./interfaces/ISafeToken.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
+import { ISafeToken } from "./interfaces/ISafeToken.sol";
 
 contract SafeToken is ISafeToken, ERC20, AccessControl {
     /*//////////////////////////////////////////////////////////////
@@ -26,12 +26,12 @@ contract SafeToken is ISafeToken, ERC20, AccessControl {
                             STATE VARIABLES
     //////////////////////////////////////////////////////////////*/
     uint256 maxMintAllocated;
-    mapping(address minter => uint256 maxMintAmount) public minterLimits;
+    mapping(address receiver => uint256 amountAllocated) public allocationLimits;
 
     /*//////////////////////////////////////////////////////////////
                                  EVENTS
     //////////////////////////////////////////////////////////////*/
-    event MinterLimitSet(address indexed minter, uint256 amount);
+    event MinterAllocationSet(address indexed minter, uint256 amount);
 
     /*//////////////////////////////////////////////////////////////
                                  ERRORS
@@ -41,44 +41,46 @@ contract SafeToken is ISafeToken, ERC20, AccessControl {
     error SAFE_YIELD__ONLY_BURNER_ROLE();
     error SAFE_YIELD__ONLY_ADMIN_ROLE();
 
-    constructor(
-        string memory name,
-        string memory symbol,
-        address admin
-    ) ERC20(name, symbol) {
+    constructor(string memory name, string memory symbol, address admin) ERC20(name, symbol) {
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
     }
 
+    //! remove to param and mint to msg.sender instead
     function mint(address to, uint256 amount) public {
-        if (!hasRole(MINTER_ROLE, _msgSender()))
+        if (!hasRole(MINTER_ROLE, _msgSender())) {
             revert SAFE_YIELD__ONLY_MINTER_ROLE();
+        }
 
-        if (
-            totalSupply() + amount > MAX_SUPPLY &&
-            minterLimits[_msgSender()] < amount
-        ) revert SAFE_YIELD__MAX_SUPPLY_EXCEEDED();
+        //! either separate checks with different custom errors or make single error with params
+        if (totalSupply() + amount > MAX_SUPPLY || allocationLimits[_msgSender()] < amount) {
+            revert SAFE_YIELD__MAX_SUPPLY_EXCEEDED();
+        }
 
         _mint(to, amount);
 
-        minterLimits[_msgSender()] -= amount;
+        allocationLimits[_msgSender()] -= amount;
     }
 
-    function setMinterLimit(address minter, uint256 amount) public override {
-        if (!hasRole(DEFAULT_ADMIN_ROLE, _msgSender()))
+    function setAllocationLimit(address minter, uint256 amount) public override {
+        if (!hasRole(DEFAULT_ADMIN_ROLE, _msgSender())) {
             revert SAFE_YIELD__ONLY_ADMIN_ROLE();
+        }
 
-        if (maxMintAllocated + amount > MAX_SUPPLY)
+        //! give @param minter the minter role if it doesn't have it
+        if (maxMintAllocated + amount > MAX_SUPPLY) {
             revert SAFE_YIELD__MAX_SUPPLY_EXCEEDED();
+        }
 
         maxMintAllocated += amount;
-        minterLimits[minter] = amount;
+        allocationLimits[minter] = amount;
 
-        emit MinterLimitSet(minter, amount);
+        emit MinterAllocationSet(minter, amount);
     }
 
-    function burn(address from, uint256 amount) public {
-        if (!hasRole(BURNER_ROLE, _msgSender()))
+    function burn(address from, uint256 amount) public override {
+        if (!hasRole(BURNER_ROLE, _msgSender())) {
             revert SAFE_YIELD__ONLY_BURNER_ROLE();
+        }
         _burn(from, amount);
     }
 }
--- src/interfaces/ISafeToken.sol
+++ src/interfaces/ISafeToken.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.21;
+pragma solidity 0.8.26;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 interface ISafeToken is IERC20 {
     function mint(address to, uint256 amount) external;
 
-    function minterLimits(address minter) external returns (uint256);
+    function allocationLimits(address minter) external returns (uint256);
 
-    function setMinterLimit(address minter, uint256 amount) external;
+    function setAllocationLimit(address minter, uint256 amount) external;
+
+    function burn(address from, uint256 amount) external;
 }
