52e1b729e8775d3d3937e187a80b1c11633178e5
raiyanmook27 <raiyan.mook27@gmail.com>
2024-06-10 05:23:37 +0000

    QA

--- test/mocks/SafeYieldRewardDistributorMock.sol
+++ test/mocks/SafeYieldRewardDistributorMock.sol
@@ -5,11 +5,6 @@ pragma solidity 0.8.26;
 import { SafeYieldRewardDistributor } from "src/SafeYieldRewardDistributor.sol";
 
 contract SafeYieldRewardDistributorMock is SafeYieldRewardDistributor {
-    /**
-     * distributor = new SafeYieldRewardDistributor(
-     *             address(safeToken), address(usdc), teamOperations, usdcBuyback, address(staking), address(twap)
-     *         );
-     */
     constructor(address safe, address usdc, address Operations, address usdcBuyBack, address staking, address twap)
         SafeYieldRewardDistributor(safe, usdc, Operations, usdcBuyBack, staking, twap)
     { }
