1b608f8e014d0f637e3ad189e8a3b6af5ad843fe
raiyanmook27 <raiyan.mook27@gmail.com>
2024-07-08 11:34:58 +0000

    chore: trading

---
+++ src/trading/gmx/types/GMXMarketTypes.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.26;
+
+struct MarketProps {
+    address marketToken;
+    address indexToken;
+    address longToken;
+    address shortToken;
+}
