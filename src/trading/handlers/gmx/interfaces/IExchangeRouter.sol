85a0e373ab727914ce5ebcd38963b57be9199ae7
raiyanmook27 <raiyan.mook27@gmail.com>
2024-07-19 17:57:03 +0000

    chore: add additional functions in exchangeRouter interface

--- src/trading/handlers/gmx/interfaces/IExchangeRouter.sol
+++ src/trading/handlers/gmx/interfaces/IExchangeRouter.sol
@@ -5,26 +5,22 @@ pragma solidity 0.8.26;
 import { CreateDepositParams, CreateWithdrawalParams, SetPricesParams, CreateOrderParams } from "../types/GMXTypes.sol";
 
 interface IExchangeRouter {
+    function sendWnt(address receiver, uint256 amount) external payable;
+
+    // @dev Sends the given amount of tokens to the given address
+    function sendTokens(address token, address receiver, uint256 amount) external payable;
+
     /**
-     *  @dev To create a deposit, tokens need to first be transferred to the DepositVault
-     *  ExchangeRouter.createDeposit can then be called after. The transfer of tokens
-     *  and the calling of ExchangeRouter.createDeposit should be done in a single transaction.
+     * @dev Receives and executes a batch of function calls on this contract.
      */
-    function createDeposit(CreateDepositParams calldata params) external payable returns (bytes32);
-
-    function cancelDeposit(bytes32 key) external payable;
-
-    function createWithdrawal(CreateWithdrawalParams calldata params) external payable returns (bytes32);
-
-    function cancelWithdrawal(bytes32 key) external payable;
-
+    function multicall(bytes[] calldata data) external payable returns (bytes[] memory results);
     /**
      * @dev To create a swap / increase position order, collateral needs to first be transferred to the OrderVault,
-     *  ExchangeRouter.openStrategy can then be called after. The transfer of tokens for collateral and
+     *  ExchangeRouter.createOrder can then be called after. The transfer of tokens for collateral and
      *  the calling of ExchangeRouter.openStrategy should be done in a single transaction,
      *  otherwise the tokens may be transferred out by other users.
      */
-    function openStrategy(CreateOrderParams calldata params) external payable returns (bytes32);
+    function createOrder(CreateOrderParams calldata params) external payable returns (bytes32);
 
     function updateOrder(
         bytes32 key,
