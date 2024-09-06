228cb88806806467097dea09b44b45e1ea8d22cc
raiyanmook27 <raiyan.mook27@gmail.com>
2024-09-06 06:44:42 +0000

    QA fixes airdrop contract

--- src/SafeYieldAirdrop.sol
+++ src/SafeYieldAirdrop.sol
@@ -3,6 +3,8 @@
 pragma solidity 0.8.26;
 
 import { ISafeYieldAirdrop } from "./interfaces/ISafeYieldAirdrop.sol";
+import { ISafeYieldStaking } from "./interfaces/ISafeYieldStaking.sol";
+import { ISafeYieldLockUp } from "./interfaces/ISafeYieldLockUp.sol";
 import { ISafeToken } from "./interfaces/ISafeToken.sol";
 import { Pausable } from "@openzeppelin/contracts/utils/Pausable.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -10,7 +12,6 @@ import { Ownable, Ownable2Step } from "@openzeppelin/contracts/access/Ownable2St
 import { MerkleProof } from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-//! DEPRECATED !!!
 contract SafeYieldAirdrop is ISafeYieldAirdrop, Ownable2Step, Pausable {
     using SafeERC20 for ISafeToken;
     /*//////////////////////////////////////////////////////////////
@@ -23,6 +24,8 @@ contract SafeYieldAirdrop is ISafeYieldAirdrop, Ownable2Step, Pausable {
                             STATE VARIABLES
     //////////////////////////////////////////////////////////////*/
     ISafeToken public sayToken;
+    ISafeYieldStaking public staking;
+    ISafeYieldLockUp public lockUp;
     mapping(address user => bool) public hasClaimed;
 
     /*//////////////////////////////////////////////////////////////
@@ -40,15 +43,21 @@ contract SafeYieldAirdrop is ISafeYieldAirdrop, Ownable2Step, Pausable {
     error SYA__INVALID_AMOUNT();
     error SYA__INVALID_MERKLE_ROOT();
     /// @notice Thrown if address has already claimed
-    error SYA__AIRDROP_CLAIMED();
+    error SYA__TOKENS_CLAIMED();
     /// @notice Thrown if address/amount are not part of Merkle tree
-    error SYA__AIRDROP_INVALID_CLAIM();
+    error SYA__INVALID_CLAIM();
 
-    constructor(address _sayToken, bytes32 _merkleRoot, address protocolAdmin) Ownable(protocolAdmin) {
-        if (_sayToken == address(0) || protocolAdmin == address(0)) revert SYA_INVALID_ADDRESS();
+    constructor(address _sayToken, address _lockUp, address _staking, bytes32 _merkleRoot, address protocolAdmin)
+        Ownable(protocolAdmin)
+    {
+        if (_sayToken == address(0) || protocolAdmin == address(0) || _lockUp == address(0) || _staking == address(0)) {
+            revert SYA_INVALID_ADDRESS();
+        }
         if (_merkleRoot == bytes32(0)) revert SYA__INVALID_MERKLE_ROOT();
 
         sayToken = ISafeToken(_sayToken);
+        staking = ISafeYieldStaking(_staking);
+
         merkleRoot = _merkleRoot;
     }
 
@@ -56,25 +65,33 @@ contract SafeYieldAirdrop is ISafeYieldAirdrop, Ownable2Step, Pausable {
         if (amount == 0) revert SYA__INVALID_AMOUNT();
         if (merkleProof.length == 0) revert SYA__INVALID_PROOF();
 
-        if (hasClaimed[msg.sender]) revert SYA__AIRDROP_CLAIMED();
+        if (hasClaimed[msg.sender]) revert SYA__TOKENS_CLAIMED();
 
         if (!MerkleProof.verify(merkleProof, merkleRoot, keccak256(abi.encodePacked(msg.sender, amount)))) {
-            revert SYA__AIRDROP_INVALID_CLAIM();
+            revert SYA__INVALID_CLAIM();
         }
 
         hasClaimed[msg.sender] = true;
-        sayToken.safeTransfer(msg.sender, amount);
+
+        lockUp.vestFor(msg.sender, amount);
+
+        sayToken.approve(address(staking), amount);
+
+        staking.stakeFor(msg.sender, uint128(amount));
 
         emit sayTokenAirdropClaimed(msg.sender, amount);
     }
 
     function mintAllSayTokens(uint256 totalAmount) external override onlyOwner {
+        if (totalAmount == 0) revert SYA__INVALID_AMOUNT();
         sayToken.mint(totalAmount);
 
         emit AllSayMinted(totalAmount);
     }
 
     function clawBackSayTokens(uint256 amount) external override onlyOwner {
+        if (amount == 0) revert SYA__INVALID_AMOUNT();
+
         sayToken.burn(address(this), amount);
 
         emit SayTokensClawedBack(msg.sender, amount);
