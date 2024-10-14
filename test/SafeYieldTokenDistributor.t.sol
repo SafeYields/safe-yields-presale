3a04cb3c9763117b7d2ef87916d5d8e219416433
raiyanmook27 <raiyan.mook27@gmail.com>
2024-10-14 12:38:20 +0000

    chore: rD broken tests fixed

--- test/SafeYieldTokenDistributor.t.sol
+++ test/SafeYieldTokenDistributor.t.sol
@@ -15,7 +15,7 @@ contract SafeYieldTokenDistributorTest is SafeYieldBaseTest {
     //////////////////////////////////////////////////////////////*/
     function testDepositRewardAsset() public startEndPresale {
         vm.prank(protocolAdmin);
-        staking.setLpAddress(makeAddr("safeYieldLP"));
+        // staking.setLpAddress(makeAddr("safeYieldLP"));
         vm.stopPrank();
 
         address[] memory rewardAssets = new address[](1);
@@ -143,7 +143,7 @@ contract SafeYieldTokenDistributorTest is SafeYieldBaseTest {
 
     function testRewardsAccrued__MultipleOps() public startEndPresale {
         vm.prank(protocolAdmin);
-        staking.setLpAddress(makeAddr("safeYieldLP"));
+        //  staking.setLpAddress(makeAddr("safeYieldLP"));
         vm.stopPrank();
 
         skip(5 minutes);
