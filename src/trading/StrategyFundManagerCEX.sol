9c4138c0638a6a9764e47b4b49af43499c765c83
Shivansh <shivansh0301@gmail.com>
2024-11-13 15:33:29 +0530

    fundmanager cex updated

--- src/trading/StrategyFundManagerCEX.sol
+++ src/trading/StrategyFundManagerCEX.sol
@@ -15,6 +15,16 @@ contract StrategyFundManagerCEX is ERC4626, Ownable {
     /*//////////////////////////////////////////////////////////////
                             STATE VARIABLES
     //////////////////////////////////////////////////////////////*/
+
+    /// @notice Maximum amount that can be deposited across all users
+    uint256 public maxTotalDeposit;
+
+    /// @notice Whether deposits are currently allowed
+    bool public depositsPaused;
+    
+    /// @notice Whether withdrawals are currently allowed
+    bool public withdrawalsPaused;
+    
     
     /// @notice Amount currently being used by strategies
     uint256 public fundsInTrading;
@@ -29,6 +39,12 @@ contract StrategyFundManagerCEX is ERC4626, Ownable {
     event StrategyFunded(address indexed strategy, uint256 amount);
     event StrategyReturned(address indexed strategy, uint256 amount, int256 pnl);
     event ControllerUpdated(address indexed newController);
+    event DepositsStatusUpdated(bool paused);
+    event WithdrawalsStatusUpdated(bool paused);
+    event MaxTotalDepositUpdated(uint256 newMax);
+    event TradingFundsWithdrawn(address indexed trader, uint256 amount);
+    event TradingFundsReturned(address indexed trader, uint256 amount, int256 pnl);
+    event DepositWithdrawalPaused();
 
     /*//////////////////////////////////////////////////////////////
                                  ERRORS
@@ -38,16 +54,21 @@ contract StrategyFundManagerCEX is ERC4626, Ownable {
     error OnlyController();
     error InsufficientAvailableFunds();
     error InvalidAmount();
+    error DepositsArePaused();
+    error WithdrawalsArePaused();
+    error MaxTotalDepositExceeded();
 
     constructor(
         IERC20 _asset,
         string memory _name,
         string memory _symbol,
+        uint256 _maxTotalDeposit,
         address _controller,
         address _owner
     ) ERC4626(_asset) ERC20(_name, _symbol) Ownable(_owner) {
         if (_controller == address(0)) revert InvalidAddress();
         controller = _controller;
+        maxTotalDeposit = _maxTotalDeposit;
     }
 
     modifier onlyController() {
@@ -67,16 +88,43 @@ contract StrategyFundManagerCEX is ERC4626, Ownable {
     }
 
     /**
-     * @notice Maximum amount that can be withdrawn (only available funds)
+     * @notice Deposits tokens into the vault
+     * @dev Overrides the default deposit function to add max cap and pause checks
      */
-    function maxWithdraw(address owner) public view override returns (uint256) {
-        uint256 availableAssets = IERC20(asset()).balanceOf(address(this));
-        return Math.min(
-            availableAssets,
-            convertToAssets(balanceOf(owner))
-        );
+    function deposit(uint256 assets, address receiver) public virtual override returns (uint256) {
+        require(!depositsPaused ,DepositsArePaused());
+        if (totalAssets() + assets > maxTotalDeposit) revert MaxTotalDepositExceeded();
+        return super.deposit(assets, receiver);
+    }
+    /**
+     * @notice Mints vault shares
+     * @dev Overrides the default mint function to add max cap and pause checks
+     */
+    function mint(uint256 shares, address receiver) public virtual override returns (uint256) {
+        require(!depositsPaused ,DepositsArePaused());
+        uint256 assets = previewMint(shares);
+        if (totalAssets() + assets > maxTotalDeposit) revert MaxTotalDepositExceeded();
+        return super.mint(shares, receiver);
+    }
+    /**
+     * @notice Withdraws tokens from the vault
+     * @dev Overrides the default withdraw function to add pause checks
+     */
+    function withdraw(uint256 assets, address receiver, address owner) public virtual override returns (uint256) {
+        if (withdrawalsPaused) revert WithdrawalsArePaused();
+        return super.withdraw(assets, receiver, owner);
     }
 
+    /**
+     * @notice Redeems vault shares
+     * @dev Overrides the default redeem function to add pause checks
+     */
+    function redeem(uint256 shares, address receiver, address owner) public virtual override returns (uint256) {
+        if (withdrawalsPaused) revert WithdrawalsArePaused();
+        return super.redeem(shares, receiver, owner);
+    }
+
+   
     /*//////////////////////////////////////////////////////////////
                         STRATEGY FUNCTIONS
     //////////////////////////////////////////////////////////////*/
@@ -93,6 +141,8 @@ contract StrategyFundManagerCEX is ERC4626, Ownable {
 
         fundsInTrading += amount;
         IERC20(asset()).safeTransfer(strategy, amount);
+        depositsPaused = true;
+        withdrawalsPaused = true;
 
         emit StrategyFunded(strategy, amount);
     }
@@ -107,14 +157,28 @@ contract StrategyFundManagerCEX is ERC4626, Ownable {
         
         fundsInTrading -= amount;
         uint256 fundsToTransfer;
-         if (pnl > 0) {
+        if (pnl > 0) {
         fundsToTransfer = amount + uint256(pnl);
-       } else {
+        }else {
         fundsToTransfer = amount - uint256(-pnl);
         }
         IERC20(asset()).safeTransferFrom(msg.sender, address(this), fundsToTransfer);
+        depositsPaused = false;
+        withdrawalsPaused = false;
 
         emit StrategyReturned(msg.sender, amount, pnl);
+    }
+    /*//////////////////////////////////////////////////////////////
+                            ADMIN FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Sets the maximum total deposit allowed
+     * @param newMax New maximum total deposit
+     */
+    function setMaxTotalDeposit(uint256 newMax) external onlyOwner {
+        maxTotalDeposit = newMax;
+        emit MaxTotalDepositUpdated(newMax);
     }
 
     /**
@@ -125,4 +189,22 @@ contract StrategyFundManagerCEX is ERC4626, Ownable {
         controller = newController;
         emit ControllerUpdated(newController);
     }
+    /**
+     * @notice Updates the withdrawal pause status
+     * @param paused New pause status
+     */
+    function setWithdrawalsPaused(bool paused) external onlyOwner {
+        withdrawalsPaused = paused;
+        emit WithdrawalsStatusUpdated(paused);
+    }
+    /**
+    * @notice Pauses both deposits and withdrawals
+    */
+    function pauseAll() external onlyOwner {
+        depositsPaused = true;
+        withdrawalsPaused = true;
+        emit DepositWithdrawalPaused();
+    }
+
+    
 }
